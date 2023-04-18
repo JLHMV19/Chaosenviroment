e2058eebac654fb94917350e6d3776ffcfd53162
JLHMV19 <jorgeluishm3456@gmail.com>
2023-04-18 17:45:11 -0500

    reparacion encontraseña de DB

--- Chaosenviroment/run.py
+++ Chaosenviroment/run.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #conexion MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'BadLands2045'
+app.config['MYSQL_PASSWORD'] = 'BadLands2045.'
 app.config['MYSQL_DB'] = 'chaossystem'
 
 mysql = MySQL(app)
