983147c3957c32173429bc55341f9d2868eb6ae9
JLHMV19 <jorgeluishm3456@gmail.com>
2023-04-18 17:28:23 -0500

    dale flaco mete la wea

--- Chaosenviroment/run.py
+++ Chaosenviroment/run.py
@@ -1,14 +1,82 @@
 #Importar
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 #Crear app medante instancia
 app = Flask(__name__)
 
+#conexion MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'BadLands2045'
+app.config['MYSQL_DB'] = 'chaossystem'
+
+mysql = MySQL(app)
+
 #Crear rutas con sus correspondientes funciones
 @app.route('/')
 def holamundo():
     return 'sistema del caos activado'
+
+#Maquina de registro
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        
+        # Crear un cursor para interactuar con la base de datos
+        cur = mysql.connection.cursor()
+        
+        # Insertar los datos en la tabla de usuarios
+        cur.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (name, email, password))
+        
+        # Guardar los cambios en la base de datos
+        mysql.connection.commit()
+        
+        # Cerrar la conexión con la base de datos
+        cur.close()
+        
+        # Redirigir al usuario a la página de inicio de sesión
+        flash('Registro exitoso. Por favor, inicie sesión.')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html')
+
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        username = request.form['username']
+        password = request.form['password']
+
+        # Verificar si los datos son correctos
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
+        user = cur.fetchone()
+        cur.close()
+
+        if user:
+            # Iniciar sesión exitosamente
+            return redirect(url_for('home'))
+        else:
+            # Mostrar un mensaje de error
+            return 'Nombre de usuario o contraseña incorrectos'
+
+    # Si el método es GET, mostrar el formulario de login
+    return '''
+        <form method="post">
+            <p>Nombre de usuario: <input type="text" name="username"></p>
+            <p>Contraseña: <input type="password" name="password"></p>
+            <p><input type="submit" value="Iniciar sesión"></p>
+        </form>
+    '''
+
 
 @app.route('/sistemadelcaos')
 def mostrarproyectos():
